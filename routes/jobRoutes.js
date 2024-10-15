c20d2dcb0101d2fbc63250f048d6275f01a04b87
MuhammedBasith <iambasith123@gmail.com>
2024-10-15 10:05:58 +0530

    Add endpoint for fetching all applicants under a job

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const axios = require("axios")
+
+// all applicants under a job
 
 // Middleware to attach the db connection to the request
 module.exports = (db) => {
@@ -29,23 +32,40 @@ module.exports = (db) => {
 
     router.post('/apply', (req, res) => {
         const jobId = req.body.jobId;
-        
-    
+
+
         // SQL query to increment the application count for the job
         const sql = `UPDATE jobs SET application_count = application_count + 1 WHERE idjobs = ?`;
-    
+
         db.query(sql, [jobId], (error, results) => {
             if (error) {
                 return res.status(500).json({ message: 'Error updating application count', error });
             }
-    
+
             if (results.affectedRows === 0) {
                 return res.status(404).json({ message: 'Job not found' });
             }
-    
+
             res.status(200).json({ message: 'Application count updated successfully' });
         });
     });
 
+
+    router.get("/:jobId/applicants", async (req, res) => {
+
+        try{
+            const jobId = req.params.jobId;
+            const applicants = await axios.get(`http://localhost:3001/users/${jobId}/getAllApplicants`)
+            const data = await applicants.data
+            res.status(200).json(data)
+
+        }catch(err){
+            res.status(500).json(err.message)
+        }
+
+    })
+
     return router;
 };
+
+
